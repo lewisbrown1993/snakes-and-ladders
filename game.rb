0e3b3fa57d218a52b18373f14ab8ca47e13c6344
Simon Smith <sds46@cantab.net>
2017-02-08 10:25:26 +0000

    pass test_number_of_players

--- game.rb
+++ game.rb
@@ -1,2 +1,7 @@
 class Game
+  attr_reader :number_of_players
+
+  def initialize(number_of_players)
+    @number_of_players = number_of_players
+  end
 end
--- specs/game_spec.rb
+++ specs/game_spec.rb
@@ -5,11 +5,15 @@ require_relative("../game.rb")
 class TestGame < MiniTest::Test
 
   def setup
-    @game = Game.new
+    @game = Game.new(2)
   end
 
 def test_can_create_game
   assert_equal(Game, @game.class())
 end
+
+def test_number_of_players
+  assert_equal(2, @game.number_of_players)
+  end
 
 end
