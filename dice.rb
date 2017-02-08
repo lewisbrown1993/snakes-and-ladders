9b28f0d4dfb35c180f398139677ec9b2f2a76027
Simon Smith <sds46@cantab.net>
2017-02-08 09:40:00 +0000

    pass test_dice_starts_with_no_value

--- dice.rb
+++ dice.rb
@@ -1,3 +1,9 @@
 class Dice
 
+  attr_reader :value
+  
+  def initialize
+    @value = nil
+  end
+
 end
--- specs/dice_spec.rb
+++ specs/dice_spec.rb
@@ -12,4 +12,8 @@ class TestDice < Minitest::Test
     assert_equal(Dice, @dice.class())
   end
 
+  def test_dice_starts_with_no_value
+    assert_nil(Dice.new.value)
+  end
+
 end
